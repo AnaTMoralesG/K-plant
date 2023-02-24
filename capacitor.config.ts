5694518c54345aaca6db1d86ad11b9004493aab8
FernandoKumul <ferdesarrollador11@outlook.com>
2023-02-23 19:54:28 -0500

    Se agrego Firebase, api clima y pagina home(sin terminar)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'k-plant',
+  appName: 'Kapa',
   webDir: 'www',
   bundledWebRuntime: false
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: '29fac49f85db9d4b93496492a2717274',
+  API_URL: 'https://api.openweathermap.org/data/2.5',
+  firebaseConfig: {
+    projectId: 'k-plant',
+    appId: '1:958087125209:web:82319f2a29220aaf8946c8',
+    databaseURL: 'https://k-plant-default-rtdb.firebaseio.com',
+    storageBucket: 'k-plant.appspot.com',
+    apiKey: 'AIzaSyButpFvcCDDwjOAjd08r8GoDGMrruIeisg',
+    authDomain: 'k-plant.firebaseapp.com',
+    messagingSenderId: '958087125209',
+  }
 };
 
 /*
